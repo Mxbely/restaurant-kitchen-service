d9f5838b83906515bebba0583f495a97494344e4
Max <mxbely98@gmail.com>
2024-11-14 18:10:09 +0200

    add kitchen models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth import settings
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    dish_type = models.ForeignKey(DishType, on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+
+    class Meta:
+        verbose_name_plural = "Dishes"
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    year_of_experience = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ("username",)
+
+    def __str__(self):
+        return self.username
+
